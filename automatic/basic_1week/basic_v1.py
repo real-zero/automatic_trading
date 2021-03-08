7b0e6496cad65381de4dfca4dbc666b5eee39787
real-zero <ddilggu3@naver.com>
2021-03-08 20:14:11 +0900

    automatic

---
+++ automatic/basic_1week/basic_v1.py
@@ -0,0 +1,13 @@
+stock = '키움증권'
+price = 3900
+move = 3.8
+
+print(stock, price, move)
+# 문자열 포매팅
+print('등락률 : %s 출력하기' %move)
+
+# for문 (시작, 끝, 크기)
+for i in range ( 1, 100,10):
+    print(i)
+    if (i >= 55):
+        break
